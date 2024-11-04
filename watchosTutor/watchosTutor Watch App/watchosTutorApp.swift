6925ebf8eeaf014caad954734014c184d87b8171
Sarvar019 <megafon.kcr.09.09@gmail.com>
2024-11-04 15:10:38 +0500

    pair wacthApp with iphone

---
+++ watchosTutor/watchosTutor Watch App/watchosTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchosTutorApp.swift
+//  watchosTutor Watch App
+//
+//  Created by Sarvar Boltaboyev on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct watchosTutor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
